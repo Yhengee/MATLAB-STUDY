9e2297dc61b4b5d06fa504d54a0f1dce378deda8
Yhengee <2022220026@email.szu.edu.cn>
2024-03-18 14:54:50 +0800

    Update Matlab code

--- study1.m
+++ study1.m
@@ -2,8 +2,8 @@ A = [1,2,3,4;5,6,7,8;9,10,11,12]
 A(:, [1 3]) = []
 
 B = [1,2,3;4,5,6;7,8,9;10,11,12]
-numRows = size(B, 1); % 获取矩阵的行数
-B(numRows-2, :) = [] % 删除倒数第三行
+numRows = size(B, 1);
+B(numRows-2, :) = []
 
 str = "I'm A STUDENT"
 lowerstr = lower(str)
@@ -11,4 +11,4 @@ lowerstr = lower(str)
 
 oldString = 'student';
 newString = 'teacher';
-result = strrep(lowerstr, oldString, newString)% 显示结果
+result = strrep(lowerstr, oldString, newString)
